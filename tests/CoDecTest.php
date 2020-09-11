edd9e4c373f09776d42424ad80d82f70bc3329f7
zamasaur <zamasaur@hotmail.com>
2020-09-11 16:04:10 +0200

    improve test

--- tests/CoDecTest.php
+++ tests/CoDecTest.php
@@ -12,23 +12,23 @@ final class CoDecTest extends TestCase
         $urlCoDec = new UrlCoDec();
         
         $this->assertEquals(
-            "Hello%20World%21",
-            $urlCoDec->encode("Hello World!")
+            "Hello%20World%21%20%E5%8F%AF%E6%84%9B%E7%9A%84%E5%B0%8F%E8%B2%93",
+            $urlCoDec->encode("Hello World! 可愛的小貓")
         );
 
         $this->assertEquals(
-            "Hello World!",
-            $urlCoDec->decode("Hello%20World%21")
+            "Hello World! 可愛的小貓",
+            $urlCoDec->decode("Hello%20World%21%20%E5%8F%AF%E6%84%9B%E7%9A%84%E5%B0%8F%E8%B2%93")
         );
 
         $this->assertEquals(
-            $urlCoDec->decode($urlCoDec->encode("Hello World!")),
-            $urlCoDec->decode("Hello%20World%21")
+            $urlCoDec->decode($urlCoDec->encode("Hello World! 可愛的小貓")),
+            $urlCoDec->decode("Hello%20World%21%20%E5%8F%AF%E6%84%9B%E7%9A%84%E5%B0%8F%E8%B2%93")
         );
 
         $this->assertEquals(
-            $urlCoDec->encode($urlCoDec->decode("Hello%20World%21")),
-            $urlCoDec->encode("Hello World!")
+            $urlCoDec->encode($urlCoDec->decode("Hello%20World%21%20%E5%8F%AF%E6%84%9B%E7%9A%84%E5%B0%8F%E8%B2%93")),
+            $urlCoDec->encode("Hello World! 可愛的小貓")
         );
     }
 
@@ -37,23 +37,23 @@ final class CoDecTest extends TestCase
         $base64CoDec = new Base64CoDec();
         
         $this->assertEquals(
-            "SGVsbG8gV29ybGQh",
-            $base64CoDec->encode("Hello World!")
+            "SGVsbG8gV29ybGQhIOWPr-aEm-eahOWwj-iykw",
+            $base64CoDec->encode("Hello World! 可愛的小貓")
         );
 
         $this->assertEquals(
-            "Hello World!",
-            $base64CoDec->decode("SGVsbG8gV29ybGQh")
+            "Hello World! 可愛的小貓",
+            $base64CoDec->decode("SGVsbG8gV29ybGQhIOWPr-aEm-eahOWwj-iykw")
         );
 
         $this->assertEquals(
-            $base64CoDec->decode($base64CoDec->encode("Hello World!")),
-            $base64CoDec->decode("SGVsbG8gV29ybGQh")
+            $base64CoDec->decode($base64CoDec->encode("Hello World! 可愛的小貓")),
+            $base64CoDec->decode("SGVsbG8gV29ybGQhIOWPr-aEm-eahOWwj-iykw")
         );
 
         $this->assertEquals(
-            $base64CoDec->encode($base64CoDec->decode("SGVsbG8gV29ybGQh")),
-            $base64CoDec->encode("Hello World!")
+            $base64CoDec->encode($base64CoDec->decode("SGVsbG8gV29ybGQhIOWPr-aEm-eahOWwj-iykw")),
+            $base64CoDec->encode("Hello World! 可愛的小貓")
         );
     }
 }
